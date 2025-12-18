be7f96fcf27910eaa4b96fd751fea226ca19c0b3
Wasem1209 <philipwasem3@gmail.com>
2025-12-18 06:36:33 +0100

    Modified the loader

--- frontend/brit/app/Components/Spinner.tsx
+++ frontend/brit/app/Components/Spinner.tsx
@@ -1,32 +1,11 @@
-"use client";
+import React from 'react'
 
-import React from "react";
 
-const Spinner = () => {
+export default function AppLoading() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-white">
-      <div
-        className="
-          w-16 h-16
-          rounded-full
-          animate-spin
-          border-4
-          border-gray-200
-          border-t-sky-400
-          border-r-gray-300
-          border-b-white
-          border-l-gray-300
-          shadow-md
-        "
-        style={{
-          borderTopColor: "rgba(14, 165, 233, 0.8)", // sky
-          borderRightColor: "rgba(156, 163, 175, 0.8)", // gray
-          borderBottomColor: "rgba(255, 255, 255, 0.8)", // white
-          borderLeftColor: "rgba(156, 163, 175, 0.8)", // gray
-        }}
-      ></div>
+    <div className="flex justify-center items-center min-h-screen bg-[rgba(0,0,0,0.5)]">
+      <span className="appLoader"></span>
+      <span className="appLoader2"></span>
     </div>
-  );
-};
-
-export default Spinner;
+  ); 
+}
