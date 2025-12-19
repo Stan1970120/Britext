9ae2b6f5aeb71f8c8b248c7aec07e47afe55744c
Wasem1209 <philipwasem3@gmail.com>
2025-12-19 17:39:10 +0100

    Added api

--- frontend/brit/app/constant/index.ts
+++ frontend/brit/app/constant/index.ts
@@ -1,3 +1,3 @@
 // constants/index.ts
 export const REST_API =
-    process.env.NEXT_PUBLIC_REST_API || "http://localhost:5000/api";
+    process.env.NEXT_PUBLIC_REST_API || "https://britext.onrender.com/api/v1";
