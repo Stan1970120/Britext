a2a0b1d4feb0ea5b16b0a76c22209fb54311d0d0
Philip Terfa Wasem <philipwasem3@gmail.com>
2025-12-19 19:24:01 +0100

    Update index.ts

--- frontend/brit/app/constant/index.ts
+++ frontend/brit/app/constant/index.ts
@@ -1,3 +1,3 @@
 // constants/index.ts
 export const REST_API =
-    process.env.NEXT_PUBLIC_REST_API || "https://britext.onrender.com/api/v1";
+    process.env.NEXT_PUBLIC_REST_API || "https://britext.onrender.com";
