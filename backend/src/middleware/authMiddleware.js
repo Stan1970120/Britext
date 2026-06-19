00b23b1d9c79e8685d455e44f68fad4682de4116
Wasem1209 <philipwasem3@gmail.com>
2026-06-19 18:16:12 +0100

    Hope

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -26,7 +26,7 @@ export const protect = (req, res, next) => {
   }
 };
 
-// ✅ Admin-only middleware
+//Admin-only middleware
 export const adminOnly = (req, res, next) => {
   if (!req.user || req.user.role !== "admin") {
     return res.status(403).json({ message: "Admins only" });
